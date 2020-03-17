7a0da8df91e7ad62543006323c1a09213e19823b
hyuckang <hyuckang15@gmail.com>
2020-03-18 00:00:06 +0900

    boj 16988(Baaaaaaaaaduk2 (Easy)) solve

--- 16988.cpp
+++ 16988.cpp
@@ -1,39 +1,20 @@
 #include<iostream>
-#include<cstring>
 #include<queue>
+#include<cstring>
 using namespace std;
-int N, M;
+int N, M, max_res = -1;
 int map[22][22];
-int enemy_map[22][22];
-int enemy_cnt;
 bool vist[22][22];
-int max_res = 0;
 int dr[] = {-1, 1, 0, 0};
 int dc[] = {0, 0, -1, 1};
-void init();
-void input();
-void pr_map();
-void pr_vist();
 
-bool is_die(int r, int c)
-{   // (r, c) 돌이 죽으면 true, 죽지 않으면 false
-    for(int dir=0; dir<4; dir++)
-    {
-        int nr = r + dr[dir];
-        int nc = c + dc[dir];
-        if(nr<0 || nr>=N || nc<0 || nc>=M) continue;
-        if(map[nr][nc] == 0) return false;
-    }
-    return true;
-}
-
-
-int calc_bfs(int r, int c)
+int calc(int r, int c)
 {
-    int cnt = 1;    
+    int cnt = 1, die_flag = 0;
     queue<pair<int, int>> Q;
     Q.push({r, c});
     vist[r][c] = true;
+    
     while(!Q.empty())
     {
         pair<int, int> cur = Q.front(); Q.pop();
@@ -42,36 +23,42 @@ int calc_bfs(int r, int c)
             int nr = cur.first + dr[dir];
             int nc = cur.second + dc[dir];
             if(nr<0 || nr>=N || nc<0 || nc>=M) continue;
+            if(map[nr][nc] == 0)
+            {
+                die_flag = 1;
+                continue;
+            }
             if(map[nr][nc] != 2 || vist[nr][nc]) continue;
             vist[nr][nc] = true;
             Q.push({nr, nc});
             cnt++;
-            if(!is_die(nr, nc)) return 0;
         }
     }
+    if(die_flag) return 0;
     return cnt;
 }
-
-
 void go(int r, int c, int cnt)
 {
+    // 죽는 돌의 갯수 계산
     if(cnt == 2)
     {
         memset(vist, false, sizeof(vist));
+        int tmp = 0;
         for(int i=0; i<N; i++)
         {
             for(int j=0; j<M; j++)
             {
                 if(map[i][j] == 2 && vist[i][j] == false)
                 {
-                    int tmp = calc_bfs(i, j);
-                    max_res = max(max_res, tmp);
-                    // pr_vist();
+                    tmp += calc(i, j);
                 }
             }
         }
+        max_res = max(max_res, tmp);
         return;
     }
+
+    // 돌 착수
     for(int i=r; i<N; i++)
     {
         for(int j=c; j<M; j++)
@@ -86,41 +73,6 @@ void go(int r, int c, int cnt)
         c = 0;
     }
 }
-void solve()
-{
-    /*
-    // 상대 돌의 집합 계산
-    enemy_cnt = 1;
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<M; j++)
-        {
-            if(map[i][j] == 2 && enemy_map[i][j] == 0)
-            {
-                dfs(i, j, enemy_cnt);
-                enemy_cnt++;
-            }
-        }
-    }
-    
-    enemy_cnt--;
-    cout<<"enemy cnt : "<<enemy_cnt<<"\n";
-    pr_enemy();
-    */
-
-
-    // 돌 2개 착수
-    go(0, 0, 0);
-
-    cout<<"max_res : "<<max_res;
-
-}
-int main()
-{
-    input();
-    solve();
-    return 0;
-}
 void input()
 {
     cin>>N>>M;
@@ -132,28 +84,14 @@ void input()
         }
     }
 }
-void pr_map()
+void solve()
 {
-    cout<<"=== map ===\n";
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<M; j++)
-        {
-            cout<<map[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
+    go(0, 0, 0);
+    cout<<max_res;
 }
-void pr_vist()
+int main()
 {
-    cout<<"=== vist ===\n";
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<M; j++)
-        {
-            if(vist[i][j]) cout<<"1 ";
-            else cout<<"0 ";
-        }
-        cout<<"\n";
-    }
+    input();
+    solve();
+    return 0;
 }
