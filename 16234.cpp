6400cf43f5a186e5e548ecf161ef167f336b5ca5
hyuckang <hyuckang15@gmail.com>
2020-03-27 22:09:46 +0900

    boj 16234(인구이동) solve

--- 16234.cpp
+++ 16234.cpp
@@ -1,80 +1,102 @@
+/*
+    1. 인구 이동이 일어나는지 검사
+    2. bfs로 연합 구하기
+    3. 인구 이동 -> 연합을 vector에 넣어두고, bfs가 끝날 때 마다 이동시킨다
+*/
 #include<iostream>
+#include<vector>
+#include<queue>
 #include<cstring>
 using namespace std;
-int N, L, R;
+int N, L, R, res = 0;
 int map[55][55], vist[55][55];
-int dr[] = {-1, 1, 0, 0};
-int dc[] = {0, 0, -1, 1};
-void input();
-void pr_vist();
+int dr[] = {-1, 1, 0, 0}, dc[] = {0, 0, -1, 1};
 
-void move_people()
-{
-    
-}
-bool is_open()
-{
-    // 국경이 열린다면 true, 열리지 않는다면 false
-    for(int r=0; r<N-1; r++)
-    {
-        for(int c=0; c<N-1; c++)
+bool is_move()
+{   
+    // 인구 이동이 일어나면 true, 일어나지 않으면 false
+    // 행 검사
+    for(int i=0; i<N-1; i++)
+    {   
+        for(int j=0; j<N; j++)
         {
-            int diff = abs(map[r][c] - map[r][c+1]);
-            if(L <= diff && diff <= R) return true;
-            diff = abs(map[r][c] - map[r+1][c]);
-            if(L <= diff && diff <= R) return true;
+            if(L <= abs(map[i][j] - map[i+1][j]) && abs(map[i][j] - map[i+1][j]) <= R)
+                return true;
+        }
+    }
+    // 열 검사
+    for(int i=0; i<N; i++)
+    {
+        for(int j=0; j<N-1; j++)
+        {
+            if(L <= abs(map[i][j] - map[i][j+1]) && abs(map[i][j] - map[i][j+1]) <= R)
+                return true;
         }
     }
     return false;
 }
-void dfs(int r, int c, int cnt)
+void bfs(int r, int c, int cnt)
 {
+    // 하나의 연합이 생성 될 때마다 인구를 이동시킨다.
+    int people_sum = 0;
+    vector<pair<int, int>> nations;
+
+    // 연합 구하기
+    queue<pair<int, int>> Q;
     vist[r][c] = cnt;
-    for(int dir=0; dir<4; dir++)
+    Q.push(make_pair(r, c));
+    people_sum += map[r][c];
+    nations.push_back(make_pair(r, c));
+
+    while(!Q.empty())
     {
-        int nr = r + dr[dir];
-        int nc = c + dc[dir];
-        if(nr<0 || nr>=N || nc<0 || nc>=N) continue;
-        if(vist[nr][nc] != -1) continue;
-        int diff = abs(map[r][c] - map[nr][nc]);
-        if(L <= diff && diff <= R) dfs(nr, nc, cnt); 
+        pair<int, int> cur = Q.front(); Q.pop();
+        for(int dir=0; dir<4; dir++)
+        {
+            int nr = cur.first + dr[dir];
+            int nc = cur.second + dc[dir];
+            if(nr < 0 || nr >= N || nc < 0 || nc >= N || vist[nr][nc] > -1) continue;
+            if(L <= abs(map[cur.first][cur.second] - map[nr][nc]) && abs(map[cur.first][cur.second] - map[nr][nc]) <= R)
+            {
+                vist[nr][nc] = cnt;
+                Q.push(make_pair(nr, nc));
+                nations.push_back(make_pair(nr, nc));
+                people_sum += map[nr][nc];
+            }
+        }
+    }
+
+    // 인구 이동
+    int people_avg = people_sum / nations.size();
+    for(int i=0; i<nations.size(); i++)
+    {
+        map[nations[i].first][nations[i].second] = people_avg;
     }
 }
 void solve()
 {
-    int res = 0;
     while(true)
-    {   
-        // 국경이 열리는지 검사
-        if(!is_open()) break;
+    {
+        // 인구 이동 검사
+        if(!is_move()) break;
+        res++;
 
-        // 연합 구하기
+        // 연합 구하고, 인구 이동
+        int cnt = 0;
         memset(vist, -1, sizeof(vist));
-        int nation_num = 0;
         for(int i=0; i<N; i++)
         {
             for(int j=0; j<N; j++)
             {
                 if(vist[i][j] == -1)
                 {
-                    dfs(i, j, nation_num);
-                    nation_num++;
+                    bfs(i, j, cnt);
+                    cnt++;
                 }
             }
         }
-        pr_vist();
-
-        // 인구 이동
-        break;
-        
     }
     cout<<res;
-}
-int main()
-{
-    input();
-    solve();
-    return 0;
 }
 void input()
 {
@@ -87,15 +109,9 @@ void input()
         }
     }
 }
-void pr_vist()
+int main()
 {
-    cout<<"=== map ===\n";
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<N; j++)
-        {
-            cout<<vist[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
+    input();
+    solve();
+    return 0;
 }
