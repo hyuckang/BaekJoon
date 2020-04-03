d25b39d6f7a3c43e76da8149dab36903ff26e28b
hyuckang <hyuckang15@gmail.com>
2020-04-03 18:34:10 +0900

    boj 17142(연구소 3) solve

--- 17142.cpp
+++ 17142.cpp
@@ -3,77 +3,77 @@
 #include<vector>
 #include<cstring>
 using namespace std;
+
 int N, M;
-int virus_cnt, empty_cnt;
-int min_res = 987654321;
 int map[55][55], vist[55][55];
 int dr[] = {-1, 1, 0, 0}, dc[] = {0, 0, -1, 1};
-vector<pair<int, int>> viruss;
-bool select_virus[15];
+int min_time = 987654321, empty_cnt = 0, virus_size;
+vector<pair<int, int>> virus;
 
-void input();
-void pr_map();
-void pr_vist();
-void bfs()
+int bfs(vector<int> &select_virus)
 {
-    int time = 0, empty = 0;
     memset(vist, -1, sizeof(vist));
+    int time = 0, empty_vist = 0;
     queue<pair<int, int>> Q;
-    for(int i=0; i<virus_cnt; i++)
-    {
-        vist[viruss[i].first][viruss[i].second] = 0;
-        if(select_virus[i])
-        {
-            Q.push({viruss[i].first, viruss[i].second});
-        }
+    
+    for(int i=0; i<M; i++)
+    {   
+        vist[virus[select_virus[i]].first][virus[select_virus[i]].second] = 0;
+        Q.push(make_pair(virus[select_virus[i]].first, virus[select_virus[i]].second));
     }
 
     while(!Q.empty())
     {
         pair<int, int> cur = Q.front(); Q.pop();
-        time = max(time, vist[cur.first][cur.second]);
-        if(time > min_res)
-        {   // 이미 최소 시간을 넘어감
-            return;
+        if(map[cur.first][cur.second] == 0)
+        {
+            empty_vist++;
+            time = max(time, vist[cur.first][cur.second]);
+            if(time > min_time) return -1;
         }
+
         for(int dir=0; dir<4; dir++)
         {
             int nr = cur.first + dr[dir];
             int nc = cur.second + dc[dir];
-            if(nr<0 || nr>N || nc<0 || nc>N) continue;
+            if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
             if(map[nr][nc] == 1 || vist[nr][nc] >= 0) continue;
-            
-            Q.push({nr, nc});
+
             vist[nr][nc] = vist[cur.first][cur.second] + 1;
+            Q.push(make_pair(nr, nc));
         }
     }
-}
-void go(int cnt, int idx)
-{
-    if(idx > virus_cnt) return;
-    
-    if(cnt == M)
-    {   // M개 선택 -> bfs 호출
-        bfs();
-        return;
-    }
 
-    select_virus[idx] = true;
-    go(cnt+1, idx+1);
-    select_virus[idx] = false;
-    go(cnt, idx+1);
-}
-int solve()
-{
-    go(0, 0);
-    if(min_res == 987654321) return min_res;
+    if(empty_cnt == empty_vist) return time;
     else return -1;
 }
-int main()
+void go(int idx, int cnt, vector<int> &select_virus)
 {
-    input();
-    cout<<solve();
-    return 0;
+    if(idx > virus_size) return;
+    if(cnt == M)
+    {
+        int bfs_res = bfs(select_virus);
+        if(bfs_res == -1) return;
+        else
+        {
+            min_time = min(min_time, bfs_res);
+            return;
+        }
+    }
+
+    select_virus.push_back(idx);
+    go(idx + 1, cnt + 1, select_virus);
+    select_virus.pop_back();
+    go(idx + 1, cnt, select_virus);
+}
+void solve()
+{
+    virus_size = virus.size();
+    vector<int> select_virus;
+    go(0, 0, select_virus);
+
+    if(min_time == 987654321) cout<<-1;
+    else cout<<min_time;
 }
 void input()
 {
@@ -83,39 +83,17 @@ void input()
         for(int j=0; j<N; j++)
         {
             cin>>map[i][j];
-            if(map[i][j] == 2)
+            if(map[i][j] == 0) empty_cnt++;
+            else if(map[i][j] == 2)
             {
-                viruss.push_back({i, j});
-                virus_cnt++;
-            }
-            else if(map[i][j] == 0)
-            {
-                empty_cnt++;
+                virus.push_back(make_pair(i, j));
             }
         }
     }
 }
-void pr_map()
+int main()
 {
-    cout<<"=== map ===\n";
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<N; j++)
-        {
-            cout<<map[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
-}
-void pr_vist()
-{
-    cout<<"=== vist ===\n";
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<N; j++)
-        {
-            cout<<vist[i][j]<<"\t";
-        }
-        cout<<"\n";
-    }
+    input();
+    solve();
+    return 0;
 }
