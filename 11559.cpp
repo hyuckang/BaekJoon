582f287deb74be58e0ef77bee25a4db77fa8b609
hyuckang <hyuckang15@gmail.com>
2020-04-11 23:12:50 +0900

    boj 11559(Puyo Puyo) solve

--- 11559.cpp
+++ 11559.cpp
@@ -1,114 +1,93 @@
-/*
-    1. 터질 것은 한번에 터져야 하기 때문에 맵을 돌면서 터트릴 것 map에 그린다.
-    2. 터질 것들이 정해지면 map에 .으로 그린다.
-    3. 블럭을 내린다.
-    4. 더 이상 터질것이 없을 때까지 반복한다.
-*/
 #include<iostream>
-#include<queue>
-#include<cstring>
 #include<vector>
+#include<cstring>
 using namespace std;
-
-char map[13][7];
-bool vist_map[13][7];   
-int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, 1, -1};
-int cnt = 0;            // 몇 연쇄인지 저장
+char map[15][15];
+bool vist[15][15];
+int pop_cnt = 0, dr[] = {-1, 1, 0, 0}, dc[] = {0, 0, -1, 1};
 
 void block_down()
 {
-    for(int i=1; i<7; i++)
+    for(int c=0; c<6; c++)
     {
-        for(int j=12; j>1; j--)
+        for(int r=11; r>0; r--)
         {
-            if(map[j][i] == '.' && map[j-1][i] != '.')
+            if(map[r][c] == '.' && map[r-1][c] != '.')
             {
-                swap(map[j][i], map[j-1][i]);
-                if(j<=10)
-                {
-                    j = j+2;
-                }
-                else if(j==11)
-                {
-                    j = 13;
-                }
-                else if(j==12) continue;
+                swap(map[r][c], map[r-1][c]);
+                
+                if(r <= 10) r = r+2;
+                else if(r == 10) r = 12;
+                else if(r == 11) continue;
             }
         }
     }
 }
 
-void input()
+void dfs(int r, int c, vector<pair<int, int>> &pop_blocks)
 {
-    for(int i=1; i<13; i++)
+    pop_blocks.push_back(make_pair(r, c));
+    vist[r][c] = true;
+
+    for(int dir=0; dir<4; dir++)
     {
-        for(int j=1; j<7; j++)
-        {
-            cin>>map[i][j];
-        }
+        int nr = r + dr[dir];
+        int nc = c + dc[dir];
+        if(nr < 0 || nr >= 12 || nc < 0 || nc >= 6) continue;
+        if(map[r][c] != map[nr][nc]) continue;
+        if(map[nr][nc] == '.' || vist[nr][nc]) continue;
+
+        dfs(nr, nc, pop_blocks);
     }
 }
 
 void solve()
 {
-    while(true){
-        memset(vist_map, false, sizeof(vist_map));
-        bool pop = false;
+    while(true)
+    {
+        memset(vist, false, sizeof(vist));
+        bool is_pop = false;
 
-        for(int i=1; i<=12; i++)
+        for(int r=0; r<12; r++)
         {
-            for(int j=1; j<=6; j++)
+            for(int c=0; c<6; c++)
             {
-                // 터질 블록들 찾기
-                if(map[i][j] == '.' || vist_map[i][j]) continue;
+                if(map[r][c] == '.' || vist[r][c]) continue;
+                
+                vector<pair<int, int>> pop_blocks;
+                dfs(r, c, pop_blocks);
 
-                queue<pair<int, int>> Q;
-                Q.push({i, j});  vist_map[i][j] = true;
-                vector<pair<int, int>> puyo_vector;     // 방문한 곳을 저장 할 벡터, 이후에 4개 이상 들어가면 터트릴 것 임
-                puyo_vector.push_back({i, j});
-           
-                while(!Q.empty())
+                if(pop_blocks.size() >= 4)
                 {
-                    pair<int, int> cur = Q.front(); Q.pop();
-                    for(int dir=0; dir<4; dir++)
+                    is_pop = true;  // 터졌다고 표시
+                    int block_cnt = pop_blocks.size();
+                    for(int i=0; i<block_cnt; i++)
                     {
-                        int nx = cur.first + dx[dir];
-                        int ny = cur.second + dy[dir];
-                        if(nx<=0 || nx>=13 || ny<=0 || ny>=7) continue; // 범위 넘어가는지                        
-                        // .인지, 방문했는지, 같은색 블럭인지
-                        if(map[nx][ny] == '.'|| vist_map[nx][ny] || (map[i][j] != map[nx][ny])) continue;
-                        
-                        Q.push({nx, ny}); vist_map[nx][ny] = true;
-                        puyo_vector.push_back({nx, ny});
+                        map[pop_blocks[i].first][pop_blocks[i].second] = '.';
                     }
                 }
-                
-                // 터진 블록들을 map에 . 으로 
-                if(puyo_vector.size() >= 4)
-                {
-                    for(int i=0; i<puyo_vector.size(); i++)
-                    {
-                        map[puyo_vector[i].first][puyo_vector[i].second] = '.';
-                        pop = true;
-                    }
-                }
-                
             }
         }
-
-        if(pop)
-        {
-            cnt++;
+    
+        if(is_pop)
+        {   
+            pop_cnt++;
             block_down();
         }
-        else
-        {
-            break;
-        }
-        
+        else break;
     }
-    cout<<cnt;
+    cout<<pop_cnt;
+}
+
+void input()
+{
+    for(int r=0; r<12; r++)
+    {
+        for(int c=0; c<6; c++)
+        {
+            cin>>map[r][c];
+        }
+    }
 }
 
 int main()
