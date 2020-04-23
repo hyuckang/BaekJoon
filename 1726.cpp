7990aacec3cee66f287fc93ae2e7eb7cd7da38ff
hyuckang <hyuckang15@gmail.com>
2020-04-23 18:56:48 +0900

    boj 1726(로봇) solve

--- 1726.cpp
+++ 1726.cpp
@@ -1,102 +1,91 @@
 #include<iostream>
 #include<queue>
 #include<cstring>
-#define x first
-#define y second
 using namespace std;
 
-int N,M;
-int map[111][111];
-int vist_map[111][111][2];          // 0 : 거리(명령), 1 : 방향
-int dx[] = {0, 0, 0,   0, 0, 0,  1, 2, 3,  -1, -2, -3};
-int dy[] = {1, 2, 3,  -1,-2,-3,  0, 0, 0,   0,  0,  0};
-int dir_val[5];
-pair<pair<int, int>, int> START;    // 행, 열, 방향
-pair<pair<int, int>, int> END;
-
-void make_dir_val(int i, int j)
+int N, M;
+int map[105][105], vist[105][105][5];
+int dr[] = {0, 0, 0, 1, -1}, dc[] = {0, 1, -1, 0, 0};
+struct Node
 {
-    if(vist_map[i][j][1] == 1)
-    {
-        dir_val[1]=1, dir_val[2]=3, dir_val[3]=2, dir_val[4]=2;
-    }
-    else if(vist_map[i][j][1] == 2)
-    {
-        dir_val[1]=3, dir_val[2]=1, dir_val[3]=2, dir_val[4]=2;
-    }
-    else if(vist_map[i][j][1] == 3)
-    {
-        dir_val[1]=2, dir_val[2]=2, dir_val[3]=1, dir_val[4]=3;
-    }
-    else if(vist_map[i][j][1] == 4)
-    {
-        dir_val[1]=2, dir_val[2]=2, dir_val[3]=3, dir_val[4]=1;
-    }
+    int r, c, dir;
+};
+Node START, END;
+int turn_dir(int cur_dir, int next_dir)
+{   
+    // cur_dir에서 next_dir로 바꾸기 위한 명령의 수
+    if(cur_dir == 1 && next_dir == 2) return 2;
+    else if(cur_dir == 2 && next_dir == 1) return 2;
+    else if(cur_dir == 3 && next_dir == 4) return 2;
+    else if(cur_dir == 4 && next_dir == 3) return 2;
+    
+    return 1;
 }
+void solve()
+{
+    memset(vist, -1, sizeof(vist));
 
+    queue<Node> Q;
+    vist[START.r][START.c][START.dir] = 0;
+    Q.push(START);
+
+    while(!Q.empty())
+    {
+        Node next, cur = Q.front(); Q.pop();
+        if(END.r == cur.r && END.c == cur.c && END.dir == cur.dir) break;
+
+        // Turn dir
+        // GO K는 모든 가중치가 1이나, Turn dir은 방향에 따라 가중치가 2가 될 수 도 있기 때문에
+        // 방향을 돌 때, 최소 명령이 달라질 수 있음
+        next.r = cur.r, next.c = cur.c;
+        for(int k=1; k<=4; k++)
+        {
+            int turn_res = vist[cur.r][cur.c][cur.dir] + turn_dir(cur.dir, k);
+            if(vist[cur.r][cur.c][k] == -1 || vist[cur.r][cur.c][k] > turn_res)
+            {
+                vist[cur.r][cur.c][k] = turn_res;
+                next.dir = k;
+                Q.push(next);                
+            }            
+        }
+
+        // GO k
+        int wall_flag = false;
+        for(int k=1; k<=3; k++)
+        {
+            if(wall_flag) break;
+            int nr = cur.r + dr[cur.dir] * k;
+            int nc = cur.c + dc[cur.dir] * k;
+            if(nr <= 0 || nr > N || nc <= 0 || nc > M || vist[nr][nc][cur.dir] >= 0) continue;
+            if(map[nr][nc])
+            {
+                wall_flag = true;
+                continue;
+            }
+            
+            next.r = nr, next.c = nc, next.dir = cur.dir;
+            vist[next.r][next.c][next.dir] = vist[cur.r][cur.c][cur.dir] + 1;
+            Q.push(next);
+        }
+    }
+    cout<<vist[END.r][END.c][END.dir];
+}
 void input()
 {
-    cin>>M>>N;
-    for(int i=1; i<=M; i++)
+    cin>>N>>M;
+    for(int i=1; i<=N; i++)
     {
-        for(int j=1; j<=N; j++)
+        for(int j=1; j<=M; j++)
         {
             cin>>map[i][j];
         }
     }
-    cin>>START.x.x>>START.x.y>>START.y;
-    cin>>END.x.x>>END.x.y>>END.y;
-}
-
-void solve()
-{
-    memset(vist_map, -1, sizeof(vist_map));
-    queue<pair<int, int>> Q;
-    Q.push({START.x.x, START.x.y});
-    vist_map[START.x.x][START.x.y][0] = 0;
-    vist_map[START.x.x][START.x.y][1] = START.y;
-
-    while(!Q.empty())
-    {   
-        pair<int, int> cur = Q.front(); Q.pop();
-        make_dir_val(cur.x, cur.y);
-
-        if(cur.x == END.x.x && cur.y == END.x.y)
-        {
-            cout<<vist_map[cur.x][cur.y][0] + dir_val[END.y]- 1;
-            break;
-        }
-
-        for(int dir=0; dir<12; dir++)
-        {
-            int nx = cur.x + dx[dir];
-            int ny = cur.y + dy[dir];
-            if(nx<1 || nx>M || ny<1 || ny>N) continue;
-            if(map[nx][ny] || vist_map[nx][ny][0] >-1) continue;
-            
-            if(dir%3 == 1)
-            {
-                int block_check_x = cur.x + dx[dir-1];
-                int block_check_y = cur.y + dy[dir-1];
-                if(map[block_check_x][block_check_y]) continue;
-            }
-            else if(dir%3==2)
-            {
-                int block_check_x = cur.x + dx[dir-1];
-                int block_check_y = cur.y + dy[dir-1];
-                int block_check_xx = cur.x + dx[dir-2];
-                int block_check_yy = cur.y + dy[dir-2];
-                if(map[block_check_xx][block_check_yy] || map[block_check_x][block_check_y]) continue;
-            }
-            
-            Q.push({nx, ny});
-            vist_map[nx][ny][0] = vist_map[cur.x][cur.y][0] + dir_val[dir/3+1];
-            vist_map[nx][ny][1] = dir/3 + 1;
-        }
-    }
+    cin>>START.r>>START.c>>START.dir;
+    cin>>END.r>>END.c>>END.dir;
 }
 int main()
 {
     input();
     solve();
+    return 0;
 }
