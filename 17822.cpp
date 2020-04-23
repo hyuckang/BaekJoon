2149e8082ba28e085b9437a58d7dcfdec5ec247f
hyuckang <hyuckang15@gmail.com>
2020-04-24 03:14:09 +0900

    boj 17822 -2

--- 17822.cpp
+++ 17822.cpp
@@ -35,22 +35,13 @@ void rotate(int circle_num, int dir, int k)
     int tmp[55];
     for(int i=0; i<=M; i++) tmp[i] = map[circle_num][i]; 
 
-    if(dir == 0) // 시계
+    // 시계 방향으로 k 번 돌리는 것은 반시계로 M-k 번 돌리는 것과 같다
+    if(dir == 1) k = M - k;
+    
+    for(int i=0; i<M; i++)
     {
-        for(int i=0; i<M; i++)
-        {
-            int next = ((i + k) % M) + 1;
-            // cout<<"next : "<<next<<"\n";
-            map[circle_num][next] = tmp[i + 1];
-        }
-    }
-    else // dir == 1, 반시계
-    {
-        for(int i=M; i>0; i--)
-        {
-            int next = ((i - k) % M) + 1;
-            map[circle_num][next] = tmp[M - i + 1];
-        }
+        int next = ((i + k) % M) + 1;
+        map[circle_num][next] = tmp[i + 1];
     }
 }
 void solve()
@@ -78,26 +69,29 @@ void solve()
         }
         cout<<"=== rotate map ===\n";
         pr_map();
-
-        // 숫자 지우기, bfs or dfs
+        //getchar();getchar();getchar();getchar();getchar();getchar();
+        
+        // 숫자 지우기, dfs
         bool remove_flag = false;
         memset(vist, false, sizeof(vist));
         for(int i=1; i<=N; i++)
         {
             for(int j=1; j<=M; j++)
             {
-                if(vist[i][j]) continue;
+                if(vist[i][j] || map[i][j] == -1) continue;
 
                 vector<pair<int, int>> remove;
                 dfs(i, j, remove);
-                if(remove.size() > 1)
+                if(remove.size() >= 2)
                 {
                     remove_flag = true;
                     int remove_sz = remove.size();
                     for(int i=0; i<remove_sz; i++)
                     {
                         map[remove[i].first][remove[i].second] = -1;
+                        // cout<<"remove_r : "<<remove[i].first<<", remove_c : "<<remove[i].second<<"\n";
                     }
+                    
                 }
             }
         }
@@ -107,8 +101,9 @@ void solve()
         // 숫자가 지워지 않았으면, 평균 계산 후 맵에 계산
         if(remove_flag) continue;
         
+
         vector<pair<int, int>> not_remove;
-        int map_sum = 0;
+        double map_sum = 0;
         for(int i=1; i<=N; i++)
         {
             for(int j=1; j<=M; j++)
@@ -121,12 +116,16 @@ void solve()
 
         int not_remove_sz = not_remove.size();
         double map_avg = map_sum / not_remove_sz;
+        cout<<"map_sum : "<<map_sum<<", map_avg : "<<map_avg<<"\n";
         for(int i=0; i<not_remove_sz; i++)
         {
             if(map[not_remove[i].first][not_remove[i].second] > map_avg) map[not_remove[i].first][not_remove[i].second]--;
             else if(map[not_remove[i].first][not_remove[i].second] < map_avg) map[not_remove[i].first][not_remove[i].second]++;
         }
     }
+
+    cout<<"=== !!! map !!! ===\n";
+    pr_map();
 
     int map_sum = 0;
     for(int i=1; i<=N; i++)
