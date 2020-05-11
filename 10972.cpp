55395e9bc59c3452756bbf709c6a25cd4606f56f
hyuckang <hyuckang15@gmail.com>
2020-05-12 00:31:39 +0900

    boj 10972(다음 순열) solve

---
+++ 10972.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int N, A[10005];
+
+void solve()
+{
+    if(next_permutation(A, A + N))
+    {
+        for(int i=0; i<N; i++) cout<<A[i]<<" ";
+    }
+    else cout<<-1<<"\n";
+}
+void input()
+{
+    cin>>N;
+    for(int i=0; i<N; i++) cin>>A[i];
+}
+int main()
+{
+    input();
+    solve();
+    return 0;
+}
