c8c557b50fa1ebbcc8f5799f186a236f8ef3845a
hyuckang <hyuckang15@gmail.com>
2020-06-17 13:10:11 +0900

    boj 17144(미세먼지 안녕!) solve
    simulation

--- 17144.cpp
+++ 17144.cpp
@@ -1,183 +1,121 @@
 #include<iostream>
-#include<cstring>
+#include<queue>
+#include<vector>
 using namespace std;
-int R, C, T, dust_cnt;
-int map[51][51];     
-int dust_map[51][51];   // 옆에서 얻은 먼지
-int wind_map[51][51];
-bool vist[51][51];
-int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, -1, 1};
-int air_condition_row;
 
-void go_dust(int r, int c)
+struct NODE
 {
-    int dust = map[r][c] / 5;
+    int r, c, val;
+};
+int R, C, T, map[55][55];
+int dr[] = {-1, 1, 0, 0}, dc[] = {0, 0, -1, 1};
+vector<int> cleaner;
+
+int calc_dust()
+{
+    int res = 0;
+    for(int i=0; i<R; i++)
+    {
+        for(int j=0; j<C; j++)
+        {
+            res += map[i][j];
+        }
+    }
+    res += 2;
+    return res;
+}
+void wind()
+{
+    pair<int, int> air;
+    air.first = cleaner.at(0), air.second = cleaner.at(1);
+    
+    // 1 열
+    for(int i=air.first-2; i>-1; i--)
+        map[i+1][0] = map[i][0];
+
+    for(int i=air.second+2; i<R;i++)
+        map[i-1][0] = map[i][0];
+
+    // 1 행, R-1 행
+    for(int i=1; i<C; i++)
+    {
+        map[0][i-1] = map[0][i];
+        map[R-1][i-1] = map[R-1][i];
+    }
+    
+    // C-1 열
+    for(int i=1; i<=air.first; i++)
+        map[i-1][C-1] = map[i][C-1];
+    for(int i=R-2; i>=air.second ;i--)
+        map[i+1][C-1] = map[i][C-1];
+
+    // 청정기 행
+    for(int i=C-2; i>0; i--)
+    {
+        map[air.first][i+1] = map[air.first][i];
+        map[air.second][i+1] = map[air.second][i];
+    }
+    map[air.first][1] = 0, map[air.second][1] = 0;
+}
+void spread_dust(int r, int c, int dust_val)
+{
+    int div_dust = dust_val / 5;
     int cnt = 0;
     for(int dir=0; dir<4; dir++)
     {
-        int nx = r + dx[dir];
-        int ny = c + dy[dir];
-        if(nx<=0 || nx>R || ny<=0 || ny>C) continue;
-        if(map[nx][ny] == -1) continue;
-
+        int nr = r + dr[dir];
+        int nc = c + dc[dir];
+        if(nr < 0 || nr >= R || nc < 0 || nc >= C || map[nr][nc] == -1) continue;
         cnt++;
-        dust_map[nx][ny] = dust_map[nx][ny] + dust;        
+        map[nr][nc] += div_dust;
     }
-    map[r][c] = map[r][c] - (dust * cnt);
+    map[r][c] -= (cnt * div_dust);
 }
-
-void go_wind(int r, int c)
+void dust()
 {
-    if(c == 1)
+    queue<NODE> Q;
+    NODE tmp;
+    for(int i=0; i<R; i++)
     {
-        if(r < air_condition_row-1)
+        for(int j=0; j<C; j++)
         {
-            if(map[r+1][c] == -1)
+            if(map[i][j] >= 5)
             {
-                wind_map[r+1][c] = -1;
+                tmp.r = i, tmp.c = j, tmp.val = map[i][j];
+                Q.push(tmp);
             }
-            else
-            {
-                wind_map[r+1][c] = map[r][c];
-            }
-        }
-        else if(air_condition_row < r)
-        {
-            if(map[r-1][c] == -1)
-            {
-                wind_map[r-1][c] = -1;    
-            }
-            else
-            {
-                wind_map[r-1][c] = map[r][c];
-            }
-        }
-        
-    }
-    else if(c == C)
-    {
-        if(r == 1 || r == R)
-        {
-            wind_map[r][c-1] = map[r][c];
-        }
-        else if(r <= air_condition_row-1)
-        {
-            wind_map[r-1][c] = map[r][c];
-        }
-        else if(air_condition_row <= r)
-        {
-            wind_map[r+1][c] = map[r][c];
         }
     }
-    else
+    while(!Q.empty())
     {
-        if(r == 1 || r == R)
-        {
-            wind_map[r][c-1] = map[r][c];
-        }
-        else if(r == air_condition_row-1 || r == air_condition_row)
-        {
-            wind_map[r][c+1] = map[r][c];
-        }
-        else
-        {
-            wind_map[r][c] = map[r][c];
-        }
-    }
-} 
-
-void input()
-{
-    cin>>R>>C>>T;    
-    for(int i=1; i<=R; i++)
-    {
-        for(int j=1; j<=C; j++)
-        {
-            cin>>map[i][j];
-            if(map[i][j] == -1)
-            {
-                air_condition_row = i;  // 공기청정기의 행
-            }
-        }
+        NODE tmp = Q.front(); Q.pop();
+        spread_dust(tmp.r, tmp.c, tmp.val);
     }
 }
-
 void solve()
 {
     while(T--)
     {
-        memset(vist, false, sizeof(vist));
-        memset(dust_map, 0, sizeof(dust_map));
-        memset(wind_map, 0, sizeof(wind_map));
-
-        // 퍼진 먼지가 다시 퍼지면 안 됨
-        for(int i=1; i<=R; i++)
-        {
-            for(int j=1; j<=C; j++)
-            {
-                if(map[i][j] > 0)
-                {
-                    vist[i][j] = true;
-                }
-            }
-        }
-
-        // 미세먼지 확산
-        for(int i=1; i<=R; i++)
-        {
-            for(int j=1; j<=C; j++)
-            {
-                if(map[i][j] >= 5 && vist[i][j])
-                {
-                    go_dust(i, j);
-                }
-            }
-        }
-
-        // 기존 미세먼지와 받은 미세먼지를 더 해준다
-        for(int i=1; i<=R; i++)
-        {
-            for(int j=1; j<=C; j++)
-            {   
-                map[i][j] = map[i][j] + dust_map[i][j];
-            }
-        }
-        
-        // 공기청정기 작동
-        for(int i=1; i<=R; i++)
-        {
-            for(int j=1; j<=C; j++)
-            {
-                if(map[i][j] == 0) continue;
-                else if(map[i][j] == -1)
-                {
-                    wind_map[i][j] = -1;
-                }
-                else
-                {
-                    go_wind(i, j);
-                }
-            }
-        }
-
-        memcpy(map, wind_map, sizeof(wind_map));
-
+        dust();
+        wind();
     }
-
-    dust_cnt = 0;
-    for(int i=1; i<=R; i++)
+    cout << calc_dust();
+}
+void input()
+{
+    cin >> R >> C >> T;
+    for(int i=0; i<R; i++)
     {
-        for(int j=1; j<=C; j++)
+        for(int j=0; j<C; j++)
         {
-            if(map[i][j] == -1) continue;
-            dust_cnt += map[i][j];
-        }    
+            cin >> map[i][j];
+            if(map[i][j] == -1) cleaner.push_back(i);
+        }
     }
-    cout<<dust_cnt<<"\n";
 }
 int main()
-{ 
+{
     input();
     solve();
+    return 0;
 }
