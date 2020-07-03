1a5a8502b4371847f09a2b240fbd6d6a82f7e755
hyuckang <hyuckang15@gmail.com>
2020-07-03 10:20:46 +0900

    boj 15688(수 정렬하기 5) solve
    sort, vector

---
+++ 15688.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	
+	int N;
+	vector<int> V;
+	
+	cin >> N;
+	for(int i=0; i<N; i++)
+	{
+		int input_;
+		cin >> input_;
+		V.push_back(input_);
+	}
+	
+	sort(V.begin(), V.end());
+	
+	int V_sz = V.size();
+	for(int i=0; i<V_sz; i++)
+		cout << V[i] << "\n";
+}
