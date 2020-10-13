27f941dd3f0f7d6528bd4bb27abfbcc4c0994c16
hyuckang <hyuckang15@gmail.com>
2020-10-14 01:33:59 +0900

    boj 2146(다리 만들기) solve

--- 2146.cpp
+++ 2146.cpp
@@ -1,117 +1,93 @@
-#include<iostream>
-#include<queue>
-#include<cstring>
+#include <iostream>
+#include <queue>
+#include <cstring>
 using namespace std;
-#define MAX 1011
 
-int N, island;
-int map[MAX][MAX][2];   //  0 : map, 1 : 섬의 번호
-int dist_map[MAX][MAX];
-int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, -1, 1};
-int min_bridge = 0xFFFFFF;
+int N, dr[] = {-1, 1, 0, 0}, dc[] = {0, 0, -1, 1};
+int map[105][105], island[105][105], vist[105][105];
+int min_res = 987654321;
 
-bool check_edge(int i, int j)
+int bfs(int r, int c)
 {
-    // 4면 중 바다가 있으면 true를 반환
+    queue<pair<int, int>> Q;
+    vist[r][c] = 0;
+    Q.push(make_pair(r, c));
+    
+    while(!Q.empty())
+    {
+        pair<int, int> cur = Q.front(); Q.pop();
+        if(island[cur.first][cur.second] != 0 && island[r][c] != island[cur.first][cur.second])
+            return vist[cur.first][cur.second] - 1;
+
+        for(int dir=0; dir<4; dir++)
+        {
+            int nr = cur.first + dr[dir], nc = cur.second + dc[dir];
+            if(nr < 0 || nr >= N || nc <0 || nc >= N) continue;
+            if(island[nr][nc] == 0 && vist[nr][nc] > -1) continue;
+            if(island[nr][nc] > 0 && island[nr][nc] == island[cur.first][cur.second]) continue;
+
+            Q.push(make_pair(nr, nc));
+            vist[nr][nc] = vist[cur.first][cur.second] + 1;
+        }
+    }
+    return 987654321;
+}
+bool check_edge(int r, int c)
+{
     for(int dir=0; dir<4; dir++)
     {
-        int nx = i + dx[dir];
-        int ny = j + dy[dir];
-        if(nx<=0 || nx>N || ny<=0 || ny>N) continue;
-        if(map[nx][ny][0] == 0) return true; 
+        int nr = r + dr[dir], nc = c + dc[dir];
+        if(nr < 0 || nr >= N || nc <0 || nc >= N) continue;
+        if(map[nr][nc] == 0) return true;
     }
     return false;
 }
-
-void input()
+void island_check(int cnt, int r, int c)
 {
-    cin>>N;
-    for(int i=1; i<=N; i++)
+    island[r][c] = cnt;
+    for(int dir=0; dir<4; dir++)
     {
-        for(int j=1; j<=N; j++)
-        {
-            cin>>map[i][j][0];
-        }
+        int nr = r + dr[dir], nc = c + dc[dir];
+        if(nr < 0 || nr >= N || nc <0 || nc >= N) continue;
+        if(map[nr][nc] == 0 || island[nr][nc] > 0) continue;
+        island_check(cnt, nr, nc);
     }
 }
-
 void solve()
 {
-    // 연결요소 찾기
-    for(int i=1; i<=N; i++)
+    int island_cnt = 1;
+    for(int r=0; r<N; r++)
     {
-        for(int j=1; j<N; j++)
+        for(int c=0; c<N; c++)
         {
-            if(!map[i][j][0] || map[i][j][1] != 0) continue;
-            island++;       // 위 조건을 빠져나오면 새로운 섬을 찾음
-
-            queue<pair<int, int>> Q;
-            Q.push({i, j});
-            map[i][j][1] = island;
-            while(!Q.empty())
-            {
-                pair<int, int> cur = Q.front(); Q.pop();
-                for(int dir=0; dir<4; dir++)
-                {
-                    int nx = cur.first + dx[dir];
-                    int ny = cur.second + dy[dir];
-                    if(nx<=0 || nx>N || ny<=0 || ny>N) continue;
-                    if(!map[nx][ny][0] || map[nx][ny][1] != 0) continue;
-                    Q.push({nx,ny});
-                    map[nx][ny][1] = island;
-                }
-            }
+            if(map[r][c] == 1 && island[r][c] == 0)
+                island_check(island_cnt++, r, c);
         }
     }
 
-    // 다리 만들기
-    for(int i=1; i<=N; i++)
+    for(int r=0; r<N; r++)
     {
-        for(int j=1; j<=N; j++)
+        for(int c=0; c<N; c++)
         {
-            if(!map[i][j][0] || !check_edge(i,j)) continue; // 바다이거나, 4면 중 바다가 없으면 continue 
-            
-            memset(dist_map, -1, sizeof(dist_map));
-            queue<pair<int, int>> bridge_Q;
-            bridge_Q.push({i, j});
-            dist_map[i][j] = 0;
-
-            while(!bridge_Q.empty())
+            if(map[r][c] == 1 && check_edge(r, c))
             {
-                pair<int, int> cur = bridge_Q.front(); bridge_Q.pop();
-                
-                if(dist_map[cur.first][cur.second] > min_bridge) break;
-
-                for(int dir=0; dir<4; dir++)
-                {
-                    int nx = cur.first + dx[dir];
-                    int ny = cur.second + dy[dir];
-                    if(nx<=0 || nx>N || ny<=0 || ny>N) continue;    // 범위를 벗어나는 경우
-                    if(dist_map[nx][ny] >= 0) continue;             // 이미 방문한 경우
-                    if(map[nx][ny][0])
-                    {
-                        if(map[i][j][1] == map[nx][ny][1]) continue;    // 같은 섬인 경우
-                        else    // 새로운 섬을 찾은 경우
-                        {
-                            if(dist_map[cur.first][cur.second] < min_bridge)
-                            {
-                                min_bridge = dist_map[cur.first][cur.second];
-                                break;
-                            }
-                        }
-                    }
-                    bridge_Q.push({nx, ny});
-                    dist_map[nx][ny] = dist_map[cur.first][cur.second] + 1;
-                    
-                }
+                memset(vist, -1, sizeof(vist));
+                min_res = min(min_res, bfs(r, c));
             }
         }
-    }
-    // 정답 출력
-    cout<<min_bridge<<"\n";
+    }     
+    cout << min_res;
 }
+void input()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);        
 
+    cin >> N;
+    for(int r=0; r<N; r++)
+        for(int c=0; c<N; c++)
+            cin >> map[r][c];
+}
 int main()
 {
     input();
