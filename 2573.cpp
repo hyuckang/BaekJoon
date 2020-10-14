d748acba4b9e4d0642986f4686285fd498c1675a
hyuckang <hyuckang15@gmail.com>
2020-10-14 23:30:43 +0900

    boj 2573(빙산) solve
    dfs

--- 2573.cpp
+++ 2573.cpp
@@ -1,93 +1,108 @@
-#include<iostream>
-#include<queue>
-#include<cstring>
+#include <iostream>
+#include <queue>
+#include <cstring>
 using namespace std;
-int N,M;
-int maps[301][301];
-bool maps_check[301][301];
-int next_maps[301][301];
-int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, -1, 1};
 
-int bfs()
+int N, M;
+int map[305][305], vist[305][305];
+int dr[] = {-1, 1, 0, 0}, dc[] = {0, 0, -1, 1};
+
+void dfs(int r, int c)
 {
-    memset(maps_check, false, sizeof(maps_check));
-    int area = 0;
-    for(int i=0;i<N;i++)
+    vist[r][c] = 1;
+    for(int dir=0; dir<4; dir++)
     {
-        for(int j=0;j<M;j++)
-        {
-            if(maps[i][j]==0 || maps_check[i][j]) continue;
-            
-            area++;
-            queue<pair<int,int>> Q;
-            Q.push({i,j});
-            maps_check[i][j] = true;
-            while(!Q.empty())
-            {
-                pair<int, int> cur = Q.front();
-                Q.pop();
-                for(int dir=0;dir<4;dir++)
-                {
-                    int nx = cur.first + dx[dir];
-                    int ny = cur.second + dy[dir];
-                    if(nx<0 || ny<0 || nx>=N || ny>=M) continue;
-                    if(maps[nx][ny]==0 || maps_check[nx][ny]) continue;
-                    
-                    Q.push({nx,ny});
-                    maps_check[nx][ny] = true;
-                }
-            }
-        }
+        int nr = r + dr[dir], nc = c + dc[dir];
+        if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+        if(map[nr][nc] == 0 || vist[nr][nc] == 1) continue;
+        dfs(nr, nc);
     }
-    if(area==0){
-        return 0;
-    }
-    return area;
 }
-int main(void)
+int seperate()
 {
-    ios::sync_with_stdio(false);
-	cin.tie(0);
-    
-    cin>>N>>M;
-    for(int i=0;i<N;i++)
+    memset(vist, 0, sizeof(vist));
+
+    int cnt = 0;
+    for(int r=0; r<N; r++)
     {
-        for(int j=0;j<M;j++)
+        for(int c=0; c<M; c++)
         {
-            cin>>maps[i][j];
-        }
-    }
-    
-    int year = 0;
-    int result;
-    do{
-        memcpy(next_maps, maps, sizeof(next_maps));
-        year++;
-        for(int i=0;i<N;i++)
-        {
-            for(int j=0;j<M;j++)
+            if(map[r][c] > 0 && vist[r][c] == 0)
             {
-                if(maps[i][j]==0) continue;
-                for(int dir=0;dir<4;dir++)
-                {   
-                    int nx = i + dx[dir];
-                    int ny = j + dy[dir];
-                    if(nx<0 || ny<0 || nx>=N || ny>=M) continue;
-                    if(maps[nx][ny]==0 && next_maps[i][j]>0)
-                        next_maps[i][j]--;
-                }
+                cnt++;
+                dfs(r, c);
             }
         }
-        memcpy(maps,next_maps,sizeof(maps));
-        result = bfs();
-        if(result==0){
-            year = 0;
+    }
+    return cnt;
+}
+bool melt()
+{
+    queue<pair<pair<int, int>, int>> Q;
+    bool melt_flag = false;
+    for(int r=0; r<N; r++)
+    {
+        for(int c=0; c<M; c++)
+        {
+            if(map[r][c] == 0) continue;
+            else
+            {
+                melt_flag = true;
+                int cnt = 0;
+                for(int dir=0; dir<4; dir++)
+                {
+                    int nr = r + dr[dir], nc = c + dc[dir];
+                    if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+                    if(map[nr][nc] == 0) cnt++;
+                }
+                Q.push(make_pair(make_pair(r, c), cnt));         
+            }
+        }
+    }
+
+    while(!Q.empty())
+    {
+        pair<pair<int, int>, int> cur = Q.front(); Q.pop();
+        map[cur.first.first][cur.first.second] -= cur.second;
+        if(map[cur.first.first][cur.first.second] < 0)
+            map[cur.first.first][cur.first.second] = 0;
+    }
+    return melt_flag;
+}
+void solve()
+{
+    int time = 0;
+    while(true)
+    {
+        time++;
+
+        if(!melt())
+        {
+            time = 0;
             break;
         }
-    }while(result<2);
+        if(seperate() >= 2)
+            break;
+    }
+    cout << time;
+}
+void input()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
     
-    cout<<year;
-    
+    cin >> N >> M;
+    for(int r=0; r<N; r++)
+    {
+        for(int c=0; c<M; c++)
+        {
+            cin >> map[r][c];
+        }
+    }
+}
+int main()
+{
+    input();
+    solve();
     return 0;
 }
