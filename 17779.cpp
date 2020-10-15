3ccbd65b8069a253cccfb31db02bcb0c48e3de98
hyuckang <hyuckang15@gmail.com>
2020-10-15 13:58:57 +0900

    boj 17779(게리맨더링 2) solve
    simul

--- 17779.cpp
+++ 17779.cpp
@@ -1,102 +1,97 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
-int N;
-int map[25][25], vote_map[25][25];
-void pr_vote();
-void vote(int x, int y, int d1, int d2)
+
+int N, A[25][25], map[25][25];
+int min_res = 987654321;
+
+int calc()
 {
-    memset(vote_map, 0, sizeof(vote_map));
+    int res[6] = {0, };
+    for(int r=1; r<=N; r++)
+        for(int c=1; c<=N; c++)
+            res[map[r][c]] += A[r][c];
+
+    sort(res + 1, res + 6);
+    return res[5] - res[1];
+}
+void area(int x, int y, int d1, int d2)
+{
+    for(int i=1; i<=N; i++)
+        for(int j=1; j<=N; j++)
+            map[i][j] = 0;
+    
+    for(int line=0; line<=d1; line++)
+    {
+        map[x + line][y - line] = 5;            // 1
+        map[x + d2 + line][y + d2 - line] = 5;  // 4
+    }
+    for(int line=0; line<=d2; line++)
+    {
+        map[x + line][y + line] = 5;            // 2
+        map[x + d1 + line][y - d1 + line] = 5;  // 3
+    }
+
+    for(int r=x+1; r<x+d1+d2; r++)
+    {
+        vector<int> pos;
+        for(int c=1; c<=N; c++)
+        {
+            if(map[r][c] == 5)
+                pos.push_back(c);
+        }
+        for(int c=pos[0]; c<=pos[1]; c++)
+            map[r][c] = 5;
+    }
 
     for(int r=1; r<=N; r++)
     {
         for(int c=1; c<=N; c++)
         {
+            if(map[r][c] == 5) continue;
             if(r < x + d1 && c <= y)
-            {
-                vote_map[r][c] = 1;
-            }
-            else if( r <= x + d2 && y < c)
-            {
-                vote_map[r][c] = 2;
-            }
-            else if( x + d1 <= r && c < y - d1 + d2)
-            {
-                vote_map[r][c] = 3;
-            }
-            else if ( x + d2 < r && y - d1 + d2 <= c)
-            {
-                vote_map[r][c] = 4;
-            }
+                map[r][c] = 1;
+            else if(r <= x + d2 && y < c)
+                map[r][c] = 2;
+            else if(x + d1 <= r && c < y - d1 + d2)
+                map[r][c] = 3;
+            else if(x + d2 < r && y - d1 + d2 <= c)
+                map[r][c] = 4;
         }
     }
-    
-    for(int d=0; d<=d1; d++)
-    {
-        vote_map[x + d][y -d] = 5; // 1
-        vote_map[x + d2 + d][y + d2 - d] = 5; // 4
-    }
-    for(int d=0; d<=d2; d++)
-    {
-        vote_map[x + d][y + d] = 5; // 2
-        vote_map[x + d1 + d][y - d1 + d] = 5; // 3
-    }
-
 }
 void solve()
 {
-    // (x, y) d1, d2 구하기
-    // 1 <= x < x+d1+d2 <= N
-    // 1 <= y-d1 < y < y+d2 <= N
-    //////////////////////////////
-
     for(int x=1; x<=N; x++)
     {
         for(int y=1; y<=N; y++)
         {
-            for(int d1=1; d1<=N; d1++)
+            for(int d1=1; (x+d1<=N)&&(1<=y-d1); d1++)
             {
-                for(int d2=1; d2<=N; d2++)
+                for(int d2=1; (x+d1+d2<=N)&&(y+d2<=N); d2++)
                 {
-                    if(x + d1 + d2 > N) continue;
-                    if(1 > y-d1) continue;
-                    if(y + d2 > N) continue;
-                    cout<<"x :"<<x<<", y : "<<y<<", d1 : "<<d1<<", d2 : "<<d2<<"\n";
-                    vote(x, y, d1, d2);
-                    pr_vote();
-                    getchar();
-                    
+                    area(x, y, d1, d2);
+                    min_res = min(min_res, calc());
                 }
-            }            
+            }
         }
     }
+    cout << min_res;
 }
 void input()
 {
-    cin>>N;
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> N;
     for(int i=1; i<=N; i++)
-    {
         for(int j=1; j<=N; j++)
-        {
-            cin>>map[i][j];
-        }
-    }
+            cin >> A[i][j];
 }
 int main()
 {
     input();
     solve();
     return 0;
-}
-void pr_vote()
-{
-    cout<<"=== vote ===\n";
-    for(int i=1; i<=N; i++)
-    {
-        for(int j=1; j<=N; j++)
-        {
-            cout<<vote_map[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
 }
