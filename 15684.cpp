047c8a6294554840dd0c1f2baad146778db6d840
hyuckang <hyuckang15@gmail.com>
2020-10-16 11:36:43 +0900

    boj 15684(사다리 조작) solve
    2nd solve

--- 15684.cpp
+++ 15684.cpp
@@ -1,102 +1,81 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int N, M, H;
-int map[31][11];
-int res = 987654321;
+int map[35][15];
+int min_res = 987654321;
 
-bool can_make_sadari(int r, int c)
+bool can_make(int r, int c)
 {
-    if(map[r][c] == 0 && map[r][c+1] == 0)
-    {
+    if(map[r][c] == 0 && map[r][c + 1] == 0)
         return true;
-    }
-    return false;
+    else
+        return false;
 }
-
-bool check_sadari()
+bool check()
 {
-    // 그대로 내려가면 true, 그렇지 않으면 false
-    for(int i=1; i<=N; i++)
+    for(int c=1; c<=N; c++)
     {
-        int c = i;
+        int start = c;
         for(int r=1; r<=H; r++)
         {
-            if(map[r][c] == 0) continue;
-            else
-            {
-                c = c + map[r][c];
-            }
+            start += map[r][start];
         }
-        if(i != c)
-        {
+        if(c != start)
             return false;
-        }
     }
     return true;
 }
-
 void go(int r, int c, int cnt)
 {
-    // check_sadari 를 먼저 할 경우 cnt가 4일 경우가 res에 들어갈 수 있음
-    if(4 <= cnt)
-    {
+    // 사다리의 개수가 4가 넘었는지 먼저 확인해야함
+    if(cnt > 3)
         return;
-    }
-    
-    if(check_sadari())
-    {   
-        // 사다리의 최솟값을 구하는 것이기 때문에 비교해주어야 함
-        if(cnt < res)
-        {
-            res = cnt;
-        }
+    if(check())
+    {
+        min_res = min(min_res, cnt);
         return;
     }
 
     for(int i=r; i<=H; i++)
     {
-	// 마지막 열은 사다리를 놓을수 없기 때문에 j는 N-1까지
+        // 마지막 세로줄에는 사다리를 놓을 수 없기에, j는 N-1까지
         for(int j=c; j<N; j++)
-        {
-            if(map[i][j] == 1 || map[i][j] == -1) continue;
-            if(can_make_sadari(i, j))
+        {            
+            if(map[i][j] == 1 || map[i][j + 1] == -1)
+                continue;
+            if(can_make(i, j))
             {
                 map[i][j] = 1;
-                map[i][j+1] = -1;
-                go(i, j, cnt+1);
+                map[i][j + 1] = -1;
+                go(i, j, cnt + 1);
                 map[i][j] = 0;
-                map[i][j+1] = 0;
+                map[i][j + 1] = 0;
             }
         }
         c = 1;
     }
 }
-
-void input()
-{
-    int a, b;
-    cin>>N>>M>>H;
-    for(int i=0; i<M; i++)
-    {
-        cin>>a>>b;
-        map[a][b] = 1;
-        map[a][b+1] = -1;
-    }
-}
-
 void solve()
 {
     go(1, 1, 0);
-    if(res != 987654321)
-    {
-        cout<<res<<"\n";
-    }
+    
+    if(min_res == 987654321)
+        cout << -1;
     else
-    {
-        cout<<"-1\n";
-    }
+        cout << min_res;
 }
-
+void input()
+{
+    cin >> N >> M >> H;
+    for(int i=0; i<M; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        map[a][b] = 1;
+        map[a][b + 1] = -1;
+    }    
+}
 int main()
 {
     input();
